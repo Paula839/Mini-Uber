0d1cfc1fa8e210e0f75d82c22dc5a10459665f37
Paula839 <Punkcf50@gmail.com>
2023-08-23 15:07:36 +0300

    Login Page

--- src/main/java/com/example/miniuber/HelloController.java
+++ src/main/java/com/example/miniuber/HelloController.java
@@ -1,98 +1,46 @@
 package com.example.miniuber;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+//import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
-    @FXML
-    private Stage stage;
-    @FXML
-    private Scene scene;
-    @FXML
-    private Parent root;
 
-    @FXML
-    private Button goButton;
 
     @FXML
     public void onGoButtonClick(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    public void onGoBackClick1(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("WelcomePage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-    @FXML
-    public void onGoBackClick2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Variables.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Variables.scene = new Scene(root);
+        Variables.stage.setScene(Variables.scene);
+        Variables.stage.show();
     }
 
 
-    @FXML
-    public void onSignupClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    public void onRegisterClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Request.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    public void onSigninClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Request.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    public void onSupportClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("SupportTicket.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    @FXML
-    public void onRequestClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Payment.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+
+
+
+
+
+
 
 }
