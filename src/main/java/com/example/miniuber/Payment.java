a9c2f57b262cf1322d7765b6030f9c2a10a115e4
Paula839 <Punkcf50@gmail.com>
2023-08-23 16:02:29 +0300

    Login Page

--- src/main/java/com/example/miniuber/Payment.java
+++ src/main/java/com/example/miniuber/Payment.java
@@ -1,4 +1,25 @@
 package com.example.miniuber;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Payment {
+    @FXML
+    ImageView credit, cash;
+
+    @FXML
+    public void onCreditClick()  {
+        if(credit.getOpacity() == 0)
+            credit.setOpacity(1);
+
+        else
+            credit.setOpacity(0);
+    }   @FXML
+    public void onCashClick()  {
+        if(cash.getOpacity() == 0)
+            cash.setOpacity(1);
+
+        else
+            cash.setOpacity(0);
+    }
 }
