43ff74777a5777412d891bf5c5597067f0fca761
Lenovo <nadaahmed2727@gmail.com>
2023-08-28 17:43:33 +0300

    added al request functionalities

---
+++ src/main/java/com/example/miniuber/Calculator.java
@@ -0,0 +1,5 @@
+package com.example.miniuber;
+
+public interface Calculator {
+    double calculate(int fromIndex, int toIndex);
+}
--- src/main/java/com/example/miniuber/HelloApplication.java
+++ src/main/java/com/example/miniuber/HelloApplication.java
@@ -21,5 +21,6 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
     }
 }
